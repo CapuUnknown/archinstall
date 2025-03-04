5d5edc296d944392631191fa142c2808776540a0
CapuUnknown <geilkecapu@gmail.com>
2025-03-04 22:05:20 +0100

    installation complete

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ main() {
 
   username
   hostname
-  user-pw
-  root-pw
   device
 
   echo "pacstrap -K /mnt base grub linux linux-firmware sof-firmware base-devel networkmanager efibootmgr neovim git --noconfirm --needed"
@@ -50,44 +48,28 @@ device() {
       break
     fi
   done
+  DEVICE="$result"
+  if [[ "$DEVICE" == "/dev/nvme"* ]]; then
 
-  if [[ "$result" == "/dev/nvme"* ]]; then
+    echo "mkfs.ext4 ""$DEVICE""p2"
+    echo "mkfs.fat -F32 ""$DEVICE""p1"
+    echo "mkswap ""$DEVICE""p3"
 
-    echo "mkfs.ext4 ""$result""p2"
-    echo "mkfs.fat -F32 ""$result""p1"
-    echo "mkswap ""$result""p3"
-
-    echo "mount ""$result""p2 /mnt"
+    echo "mount ""$DEVICE""p2 /mnt"
     echo "mkdir -pv /mnt/boot/efi"
-    echo "mount ""$result""p1 /mnt/boot/efi"
-    echo "swapon ""$result""p3"
+    echo "mount ""$DEVICE""p1 /mnt/boot/efi"
+    echo "swapon ""$DEVICE""p3"
   else
 
-    echo "mkfs.ext4 ""$result""2"
-    echo "mkfs.fat -F32 ""$result""1"
-    echo "mkswap ""$result""3"
+    echo "mkfs.ext4 ""$DEVICE""2"
+    echo "mkfs.fat -F32 ""$DEVICE""1"
+    echo "mkswap ""$DEVICE""3"
 
-    echo "mount ""$result""2 /mnt"
+    echo "mount ""$DEVICE""2 /mnt"
     echo "mkdir -pv /mnt/boot/efi"
-    echo "mount ""$result""1 /mnt/boot/efi"
-    echo "swapon ""$result""3"
+    echo "mount ""$DEVICE""1 /mnt/boot/efi"
+    echo "swapon ""$DEVICE""3"
   fi
-}
-
-root-pw() {
-  title="Root Password"
-  type=passwordbox
-  text="Select root password"
-  wt2
-  echo "$result"
-}
-
-user-pw() {
-  title="User Password"
-  type=passwordbox
-  text="Select user password"
-  wt2
-  echo "$result"
 }
 
 username() {
@@ -96,6 +78,7 @@ username() {
   text="Select username"
   wt2
   echo "$result"
+  NAME="$result"
 }
 
 hostname() {
@@ -104,33 +87,39 @@ hostname() {
   text="Select hostname"
   wt2
   echo "$result"
+  HOSTNAME="$result"
 }
 
 main
 #TODO: optional package checkbox
 cat <<REALEND >/mnt/next.sh
+#!/usr/bin/env bash
+
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
-# /etc/locale.gen
+
+sed -i "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+sed -i "s/^#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
-# /etc/locale.conf
-# hostname
-# root password
-# useradd -m -G wheel -s /bin/bash name 
-# user password
-# visudo wheel
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LC_ALL=en_US.UTF-8" >> /etc/locale.conf
+
+echo "$HOSTNAME" > /etc/hostname
+rootpw=($(whiptail --passwordbox --title "Root Password" --text "Select root password" 3>&1 1>&2 2>&3))
+echo "$USER":"$rootpw" | chpasswd
+
+useradd -m -G wheel -s /bin/bash "$NAME" 
+userpw=($(whiptail --passwordbox --title "User Password" --text "Select user password" 3>&1 1>&2 2>&3))
+echo "$NAME":"$userpw" | chpasswd
+
+sed -i "s/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
+
 systemctl enable NetworkManager
-# grub-install device
+
+grub-install "$DEVICE"
 grub-mkconfig -o /boot/grub/grub.cfg
-# cat "KEYMAP=de-latin1" > /etc/vconsole.conf
-# multilib
-# pacman
-systemctl enable sddm
-mkdir /home/"$USER"/AUR
-(cd /home/"$USER/AUR" && git clone https://aur.archlinux.org/yay.git && cd /home/"$USER"/AUR/yay && makepkg -sirc)
-yay -S librewolf-bin wtf wireguird gpu-passthrough-manager polymc vesktop galaxybudsclient-bin qdiskinfo auto-cpufreq mono-git
-# QEMU
-# SSH
+
+echo "KEYMAP=de-latin1" > /etc/vconsole.conf
 REALEND
 
 arch-chroot /mnt sh next.sh
