b9f41cccb57b7f0a3d1c0a82983b498916670688
CapuUnknown <geilkecapu@gmail.com>
2025-03-10 23:00:23 +0100

    fix TODOs

--- install.sh
+++ install.sh
@@ -134,6 +134,7 @@ rootpw() {
 
 main
 
+#TODO: enable cronie.service before running timeshift wizard
 #TODO: auto-cpufreq & other Laptop packages and utilities
 #TODO: DM, DE and browser choice
 #TODO: optional package checkbox
