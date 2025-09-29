939893b3dd0103e9b721c9067e823d9b98ee2927
CapuUnknown <geilkecapu@gmail.com>
2025-09-29 16:17:48 +0200

    fix DNS

--- test_install.sh
+++ test_install.sh
@@ -196,10 +196,11 @@ chmod 440 /etc/sudoers.d/99_wheel-nopasswd
 
 systemctl enable NetworkManager.service
 
-sed -i "s/^#DNS= /DNS=192.168.178.34/" /etc/systemd/resolved.conf
+sed -i "s/^#DNS=/DNS=192.168.178.34/" /etc/systemd/resolved.conf
 read -p ok
 cat /etc/systemd/resolved.conf
 read -p ok
+systemctl restart systemd-resolved
 
 grub-install "$DEVICE"
 grub-mkconfig -o /boot/grub/grub.cfg
